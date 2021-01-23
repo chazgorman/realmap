28b5905ef19ed59d2acc0ff2cb23c6a18999233e
cgorman <chaz.gorman@gmail.com>
2021-01-23 09:58:46 -0500

    Fix NextJS upgrade warnings.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import App, { Container } from 'next/app'
+import App from 'next/app'
+import Head from 'next/head'
 import React from 'react'
 import withApolloClient from '../src/lib/with-apollo-client'
 import { ApolloProvider } from 'react-apollo'
@@ -7,11 +8,23 @@ class MyApp extends App {
   render () {
     const { Component, pageProps, apolloClient } = this.props
     return (
-      <Container>
+      <>
+        <Head>
+          <title>Pro Cycling Map</title>
+          <meta charSet="utf-8" />
+         {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name="viewport"
+            content={
+              'user-scalable=0, initial-scale=1, ' +
+              'minimum-scale=1, width=device-width, height=device-height'
+            }
+          />
+        </Head>
         <ApolloProvider client={apolloClient}>
           <Component {...pageProps} />
         </ApolloProvider>
-      </Container>
+      </>
     )
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import Document, { Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
 
     return (
-      <html lang="en" dir="ltr" style={{ height: '100%', overflowY: 'hidden' }}>
-      <head>
-        <title>Pro Cycling Map</title>
-        <meta charSet="utf-8" />
-        {/* Use minimum-scale=1 to enable GPU rasterization */}
-        <meta
-          name="viewport"
-          content={
-            'user-scalable=0, initial-scale=1, ' +
-            'minimum-scale=1, width=device-width, height=device-height'
-          }
-        />
+      <Html lang="en" dir="ltr" style={{ height: '100%', overflowY: 'hidden' }}>
+      <Head>
         {/* PWA primary color */}
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
         />
-        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.4.0/leaflet.css"></link> */}
-
-        {/* <link rel="stylesheet" href="https://js.arcgis.com/3.27/esri/css/esri.css" /> */}
 
         <link rel="stylesheet" href="https://js.arcgis.com/4.16/esri/css/main.css" />
         <script src="https://js.arcgis.com/4.16/"></script>
@@ -32,13 +19,13 @@ class MyDocument extends Document {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css"></link>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossOrigin="anonymous" />
 
-        <script type="text/javascript" src="./public/static/attachNavbarBurgerClick.js"></script>
-      </head>
+        <script type="text/javascript" src="/public/static/attachNavbarBurgerClick.js"></script>
+      </Head>
       <body className="has-navbar-fixed-top" style={{height: '100%'}}>
         <Main />
         <NextScript />
       </body>
-      </html>
+      </Html>
     );
   }
 }
