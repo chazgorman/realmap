957d5bf9f766a9cfa159cfe11dd838a5bf234d68
cgorman <chaz.gorman@gmail.com>
2021-02-15 10:03:26 -0500

    Comments FTW to save my bacon later.

--- pages/index.js
+++ pages/index.js
@@ -18,63 +18,64 @@ export const allMsgsQuery = gql`
 }
 `
 
-export const allAppsQueryVars = {
-  skip: 0,
-  first: 10
-}
-
 function MainIndex() {
-
+  // React hook from Apollo is used to fetch data: useQuery
   const { loading, error, data } = useQuery(allMsgsQuery);
 
+  // Loading/error indicators
   if (loading) return <div className="button is-loading"></div>;
   if (error) return <p>Error</p>;
 
-  const activeMessages = useReactiveVar(activeMessageIdVar);
-  const showMobileMap = useReactiveVar(showMobileMapMode)
+  // Apollo reactive variables used to get current state;
+  const activeMessages = useReactiveVar(activeMessageIdVar); // Is there a message selected?
+  const showMobileMap = useReactiveVar(showMobileMapMode)    // Is active view a map and on a mobile device?
 
+  // Variables to hold conditional react components and style
   let mediaModal = undefined;
   let mediaModalColumn = undefined;
   let mapModalColumn = undefined;
-  let mediaListStyle = { height: '100vh', overflow: 'auto'};
+  let mediaListStyle = { height: '100vh', overflow: 'auto' };
   let navbar = <Navbar />;
+  let mapClassName = "column is-two-thirds is-hidden-mobile";
+  let mapStyle = { width: '100%', height: "100%" };
+  let headerStyle = { width: '100%', height: "20%", display: 'none' };
 
-  if(activeMessages.length > 0 && showMobileMap == false){
-        mediaModal = <MediaModal messageid={activeMessages[0]}></MediaModal>;
-        mediaModalColumn = (
-            <div className="is-centered is-vcentered">
-              {mediaModal}
-            </div>
-        );
-        mediaListStyle = { height: '100vh', overflow: 'auto', display: 'none'};
+  // There is an active message, and we're on desktop; 
+  if (activeMessages.length > 0 && showMobileMap == false) {
+    // Create modal to display image/media
+    mediaModal = <MediaModal messageid={activeMessages[0]}></MediaModal>;
+    mediaModalColumn = (
+      <div className="is-centered is-vcentered">
+        {mediaModal}
+      </div>
+    );
+    mediaListStyle = { height: '100vh', overflow: 'auto', display: 'none' };
   }
-
-  var mapClassName = "column is-two-thirds is-hidden-mobile";
-  var mapStyle = { width: '100%', height: "100%" };
-  var headerStyle = { width: '100%', height: "20%", display: 'none' };
-
-  if(showMobileMap){
+  // Mobile map mode implies that there is an active message; set up modal to display map
+  else if (showMobileMap) {
     mapClassName = "modal column"
     mapStyle = { width: '100%', height: "100%" };
     headerStyle = { width: '100%' };
-    mediaListStyle = { height: '100vh', overflow: 'auto', display: 'none'};
+    mediaListStyle = { height: '100vh', overflow: 'auto', display: 'none' };
     mapModalColumn = <MapModalHeader messageid={activeMessages[0]}></MapModalHeader>
     mediaModalColumn = undefined;
     navbar = <div></div>;
   }
 
   return (
-
+    // As per Bulma.io docs, 'columns' are only activated on tablet devices and above;
+    // Mobile devices will have columns stacked vertically.
+    // When in mobile map mode, elements in the first column are undefined or hidden, so 2nd column takes up full display.
     <div>
       {navbar}
       <div className="columns is-gapless is-desktop" style={{ width: '100%', height: '100%', paddingTop: '1.0rem' }}>
         <div className="column" style={{ height: '100vh', padding: '0' }}>
           {/* {searchInput}
             {topicChooser} */}
-            {mediaModalColumn}
-            <div style={mediaListStyle}>
-              <ClientMediaList mapMarkers={data.geomessages_last_14_days} />
-            </div>);
+          {mediaModalColumn}
+          <div style={mediaListStyle}>
+            <ClientMediaList mapMarkers={data.geomessages_last_14_days} />
+          </div>);
         </div>
         <div className={mapClassName}>
           <div id="mobileMapHeader" style={headerStyle}>
