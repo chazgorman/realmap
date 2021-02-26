8f79b1d96935fbd9612df60d921420fcb314ac31
cgorman <chaz.gorman@gmail.com>
2021-02-26 08:07:49 -0500

    Work in progress using embedded tweets library.

--- pages/tweetwall/index.js
+++ pages/tweetwall/index.js
@@ -7,7 +7,6 @@ import { useQuery, gql, useReactiveVar } from '@apollo/client';
 import { activeMessageIdVar, showMobileMapMode } from '../../src/appstate/cache'
 import MapController from '../../src/components/MapController'
 import MapModalHeader from '../../src/components/MapModalHeader'
-import EmbeddedTweet from '../../src/components/EmbeddedTweet'
 
 //import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
@@ -22,7 +21,7 @@ export const allMsgsQuery = gql`
 }
 `
 
-function MediaWall() {
+function Media() {
   // React hook from Apollo is used to fetch data: useQuery
   const { loading, error, data } = useQuery(allMsgsQuery);
 
@@ -40,51 +39,31 @@ function MediaWall() {
   let mapModalColumn = undefined;
   let mediaListStyle = { height: '100vh', overflow: 'auto' };
   let navbar = <Navbar />;
-  let mapClassName = "column is-two-thirds is-hidden-mobile is-vcentered";
+  let mapClassName = "column is-two-thirds is-hidden-mobile";
   let mapStyle = { width: '100%', height: "100%" };
   let headerStyle = { width: '100%', height: "20%", display: 'none' };
 
   // There is an active message, and we're on desktop; 
-  // if (activeMessages.length > 0 && showMobileMap == false) {
-  //   // Create modal to display image/media
-  //   mediaModal = <MediaModal messageid={activeMessages[0]}></MediaModal>;
-  //   var tweetEmbed = <TwitterTweetEmbed tweetId={activeMessages[0]} />;
-  //   mediaModalColumn = (
-  //     <div className="is-centered is-vcentered">
-  //       {mediaModal}
-  //       {tweetEmbed}
-  //     </div>
-  //   );
-  //   mediaListStyle = { height: '100vh', overflow: 'auto', display: 'none' };
-  // }
-  // // Mobile map mode implies that there is an active message; set up modal to display map
-  // else if (showMobileMap) {
-  //   mapClassName = "modal column"
-  //   mapStyle = { width: '100%', height: "100%" };
-  //   headerStyle = { width: '100%' };
-  //   mediaListStyle = { height: '100vh', overflow: 'auto', display: 'none' };
-  //   mapModalColumn = <MapModalHeader messageid={activeMessages[0]}></MapModalHeader>
-  //   mediaModalColumn = undefined;
-  //   navbar = <div></div>;
-  // }
-
-  // let activeMsgId = data.messages[0].message_id;
-  // if(activeMessages.length > 0){
-  //   activeMsgId = activeMessages[0].message_id;
-  // }
-  // let tweetEmbed = <TwitterTweetEmbed tweetId={activeMsgId} />
-
-  // {activeMessages.length > 0 ? <TwitterTweetEmbed tweetId={activeMessages[0].message_id} /> : 
-  //                             <TwitterTweetEmbed tweetId={data.messages[0].message_id} />}
-  // let tweetList = undefined;
-  // let i = 0;
-  // if(data.messages !== undefined){    
-  //   tweetList = data.messages.map(msg => {
-  //     return (
-  //       <TwitterTweetEmbed key={i++} tweetId={msg.message_id} />
-  //     )
-  //   });
-  // }
+  if (activeMessages.length > 0 && showMobileMap == false) {
+    // Create modal to display image/media
+    mediaModal = <MediaModal messageid={activeMessages[0]}></MediaModal>;
+    mediaModalColumn = (
+      <div className="is-centered is-vcentered">
+        {mediaModal}
+      </div>
+    );
+    mediaListStyle = { height: '100vh', overflow: 'auto', display: 'none' };
+  }
+  // Mobile map mode implies that there is an active message; set up modal to display map
+  else if (showMobileMap) {
+    mapClassName = "modal column"
+    mapStyle = { width: '100%', height: "100%" };
+    headerStyle = { width: '100%' };
+    mediaListStyle = { height: '100vh', overflow: 'auto', display: 'none' };
+    mapModalColumn = <MapModalHeader messageid={activeMessages[0]}></MapModalHeader>
+    mediaModalColumn = undefined;
+    navbar = <div></div>;
+  }
 
   return (
     // As per Bulma.io docs, 'columns' are only activated on tablet devices and above;
@@ -92,7 +71,7 @@ function MediaWall() {
     // When in mobile map mode, elements in the first column are undefined or hidden, so 2nd column takes up full display.
     <div>
       {navbar}
-      <div className="columns is-gapless is-centered is-desktop" style={{ width: '100%', height: '100%', paddingTop: '1.0rem' }}>
+      <div className="columns is-gapless is-desktop" style={{ width: '100%', height: '100%', paddingTop: '1.0rem' }}>
         <div className="column" style={{ height: '100vh', padding: '0' }}>
           {/* {searchInput}
             {topicChooser} */}
@@ -106,9 +85,8 @@ function MediaWall() {
             {mapModalColumn}
           </div>
           <div id="map" style={mapStyle}>
-            <div className="centerContent">
-              <EmbeddedTweet messageid={activeMessages[0]} />
-            </div>
+            <DynamicMap points={data}></DynamicMap>
+            <MapController map={this}></MapController>
           </div>
         </div>
       </div>
@@ -116,4 +94,4 @@ function MediaWall() {
   )
 }
 
-export default MediaWall;
+export default Media;
