62986f5bca3d3e55f2098e953845622bce90ebb6
chazgorman <chaz.gorman@gmail.com>
2021-07-29 10:48:25 -0400

    Update next.config.js
    Change hasura URL to use .net domain.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     target: 'serverless',
     env: {
-        NEXT_PUBLIC_PCM_GRAPHQL_URL: 'https://gcm.procyclingmap.com/v1/graphql',
+        NEXT_PUBLIC_PCM_GRAPHQL_URL: 'https://gcm.procyclingmap.net/v1/graphql',
       },
 }
